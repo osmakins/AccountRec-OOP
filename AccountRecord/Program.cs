c13562f9a0e493a3b50e4b874aa84999f98baa4e
M.O Akindayo <matthewakinse@gmail.com>
2018-11-21 14:59:19 +0200

    Add sample use of method and predicate

--- AccountRecord/Program.cs
+++ AccountRecord/Program.cs
@@ -16,8 +16,19 @@ namespace AccountRecord
             accountList.Add(new CheckingAccount("Grit", 0));
             accountList.Add(new CheckingAccount("Josh", 500));
 
+            Predicate<AccountBase> accountPredicate = new Predicate<AccountBase>(FindAccount);
+
+            AccountBase accountBase = accountList.Find(a => FindAccount(a));
+
+            Console.WriteLine("Account owner is: {0}", accountBase.getAccountOwner() );
+
             Application.Run(new AccountRecords(accountList));
             Console.ReadKey();
+        }
+
+        public static bool FindAccount(AccountBase A)
+        {
+            return A.getAccountOwner() == "Duke";
         }
     }
 }
